49e09eabac1bdb58a8775d96167c71ab613fe004
Antonio Wilson <antonio@ncstore.biz>
2022-12-08 20:55:24 -0500

    Continue model construction, begin template formation

--- ordering_form/settings.py
+++ ordering_form/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_app'
+    'company_app',
+    'order_app'
 ]
 
 MIDDLEWARE = [
--- ordering_form/urls.py
+++ ordering_form/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', include('user_app.urls')),
+    path('products/', include('order_app.urls')),
+    path('companies/', include('company_app.urls'))
 ]
